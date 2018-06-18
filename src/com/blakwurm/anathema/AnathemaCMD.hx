b01516e0b58120bfad05aa37f226c385ccacec3c
Alex Chythlook <achythlook@gmail.com>
2018-06-17 21:08:45 -0800

    Refactor app to basics.

--- src/com/blakwurm/anathema/AnathemaCMD.hx
+++ src/com/blakwurm/anathema/AnathemaCMD.hx
@@ -2,14 +2,18 @@ package com.blakwurm.anathema;
 
 import tink.Cli;
 import com.blakwurm.djala.System;
+import com.blakwurm.djala.Server;
+import com.blakwurm.djala.Registry;
 
 class AnathemaCMD {
     public static function main() {
         trace("Starting thing!");
-        Cli.process(Sys.args(), new SystemArgs(function (sys: System) {
+        var systemArgs = new SystemArgs(function (sys: System) {
             sys.modules.set(ServerSystemModule, new ServerSystemModule({}));
             sys.modules.set(RegistrySystemModule, new RegistrySystemModule({}));
-        }));
+        });
+        systemArgs.server = true;
+        Cli.process(Sys.args(), systemArgs);
         trace("Done Starting Thing!");
     }
 }
--- src/com/blakwurm/djala/Ui.hx
+++ src/com/blakwurm/djala/Ui.hx
@@ -2,33 +2,16 @@ package com.blakwurm.djala;
 
 import com.blakwurm.djala.System;
 import com.blakwurm.djala.Registry;
-import haxe.ui.HaxeUIApp;
-import haxe.ui.Toolkit;
-import haxe.ui.components.Button;
-import haxe.ui.core.Component;
-import haxe.ui.core.Screen;
-import haxe.ui.macros.ComponentMacros;
 
 class Ui implements SystemModule {
-    private var app : HaxeUIApp;
     public function preInit(system: System): Bool {
         if (!system.systemArgs.headless) {
-            Toolkit.init();
-            app = new HaxeUIApp();
             
         }
         return true;
     }
     public function init(system: System): Bool {
-        if (!system.systemArgs.headless) {
-            var main:Component;
-            if (system.systemArgs.mobile) {
-                main = ComponentMacros.buildComponent("resources/ui/shell-mobile.xml");
-            } else {
-                main = ComponentMacros.buildComponent("resources/ui/shell-desktop.xml");
-            }
-            app.addComponent(main);
-        }
+        
         return true;
     }
     public function postInit(system: System): Bool return true;
